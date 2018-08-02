f609c278119336e59c3478ee2ebe1e743c4daa7b
Mary Brady <emceebrady@gmail.com>
2018-08-02 10:45:01 -0600

    set functions

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const mrSnibbly = {
     name: 'mr. snibbly',
     moods: [
         'classy', 
-        'sassy'
+        'sassy',
+        'gone'
     ],
     moodImg: [
         'assets/fancycat.png',
-        'assets/meancat.jpg'
+        'assets/meancat.jpg',
+        ''
     ],
     tolerance: 7,
     pets: 0,
@@ -23,10 +25,17 @@ function draw() {
     catName.innerText = mrSnibbly.name;
     mood.innerText = mrSnibbly.moods[mrSnibbly.moodIndex];
     pets.innerText = mrSnibbly.pets.toString();
+    if(mrSnibbly.pet > mrSnibbly.tolerance * 2) {
+        document.getElementById("pet-button").disabled = true;
+    }
 }
 
 function pet() {
-
+    mrSnibbly.pets++;
+    if(mrSnibbly.pets % mrSnibbly.tolerance == 0) {
+        mrSnibbly.moodIndex++; 
+    }
+    draw();
 }
 
 function reset() {
